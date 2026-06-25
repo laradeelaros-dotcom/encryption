f0b03c8d6e318d1a72ef7ecb5002efb2434c5000
Lara Santiago <laradeelaros-dotcom>
2026-06-25 15:22:23 -0300

    feat: Return to the beginning if you exceed Z.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const resultField = document.getElementById("outputResult");
 function encryptText (baseText, shiftKey ) {
     let resultText = "";
 
+    baseText = baseText.toUpperCase();//Transformar o texto em maiúsculo
+
 //Percorrer cada letra do texto
     for (let i = 0; i < baseText.length; i++) {
         let currentLetter = baseText[i];
@@ -17,7 +19,8 @@ function encryptText (baseText, shiftKey ) {
             resultText += " "
         } else {
             let codeAscii = baseText.charCodeAt(i);
-            let codeNewAscii = codeAscii + shiftKey;
+
+            let codeNewAscii = (codeAscii - 65 + shiftKey) % 26 +65; //forçar o código ASCII a ficar dentro do intervalo das letras maiúsculas (A-Z) e retornar ao início do alfabeto caso ultrapasse o Z
             let newLetter = String.fromCharCode(codeNewAscii);
 
             resultText += newLetter;
@@ -26,3 +29,17 @@ function encryptText (baseText, shiftKey ) {
     }
     return resultText;
 }
+
+encryptButton.addEventListener('click', function () {
+   const typedText = textField.value;
+   const keyValue = parseInt(keyField.value) //Capturar o valor digitado e transformar em números "parseInt"
+
+//Se o valor da chave não for válido
+    if (isNaN(keyValue)) {
+        console.error("ERRO: A chave digitada não é um número válido!");
+        resultField.innerText = "Por favor, digite um número válido para a chave."
+        return;
+    }
+    const finalResult = encryptText(typedText, keyValue);
+    resultField.innerText = finalResult;
+});

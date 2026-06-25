af2b9fe6e4ef417425281c23574e65cc255ca5ad
Lara Santiago <laradeelaros-dotcom>
2026-06-25 14:16:39 -0300

    feat: loop for and does not encrypt whitespace

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// 1- Capturar os elementos do HTML utilizando os IDs
+//Capturar os elementos do HTML utilizando os IDs
 const textField = document.getElementById("textarea");
 const keyField = document.getElementById("numberKey");
 const encryptButton = document.getElementById("button_encrypt");
 const decryptButton = document.getElementById("button_decrypt");
 const resultField = document.getElementById("outputResult");
 
-// 2- Informar que o botão "encryptButton" fará um evento de click
-encryptButton.addEventListener('click', function() {
+function encryptText (baseText, shiftKey ) {
+    let resultText = "";
 
-    //capturar o que o usuario digitou
-    const userText = textField.value;
+//Percorrer cada letra do texto
+    for (let i = 0; i < baseText.length; i++) {
+        let currentLetter = baseText[i];
 
-    //Exibir mensagem teste
-    console.log("botão digitado com sucesso");
-    console.log("texto digitado:", userText);
-}) 
+//Se for um espaço adiciona ao resultado sem altera-lo
+        if (currentLetter === " "){
+            resultText += " "
+        } else {
+            let codeAscii = baseText.charCodeAt(i);
+            let codeNewAscii = codeAscii + shiftKey;
+            let newLetter = String.fromCharCode(codeNewAscii);
+
+            resultText += newLetter;
+        }    
+
+    }
+    return resultText;
+}
